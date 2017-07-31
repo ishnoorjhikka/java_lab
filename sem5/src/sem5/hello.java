3a2c87524d47027a800e70866a03d8a3798620e8
ishnoorjhikka <ishnoorjhikka@gmail.com>
2017-07-31 16:40:32 +0530

    initiAL commit

---
+++ sem5/src/sem5/hello.java
@@ -0,0 +1,17 @@
+package sem5;
+
+public class hello {
+
+	public static void main(String args[])
+	{
+		System.out.print("hey");
+		int i=0,j=0;
+		for(i=0;i<10;i++)
+			{
+			for(j=0;j<i;j++)
+			     System.out.print("*");
+			System.out.print("\n");
+			}
+	}
+	
+}
